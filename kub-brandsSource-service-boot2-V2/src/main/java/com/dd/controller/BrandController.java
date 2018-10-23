544062f9cc370c91950eaaab67c410ff4b64b0c3
das <das@das-HP-Pavilion>
2018-10-22 23:34:46 -0700

    added outsidecall

--- kub-brandsSource-service-boot2-V2/src/main/java/com/dd/controller/BrandController.java
+++ kub-brandsSource-service-boot2-V2/src/main/java/com/dd/controller/BrandController.java
@@ -8,12 +8,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.dd.dao.ConfigDAO;
 import com.dd.dao.DAOException;
@@ -60,6 +64,26 @@ public class BrandController {
 		AppStatusInfo asi =getAppStatus();
        	logger.info("## BrandController.asi:" + asi);
         return getHTML(asi,null);
+    }
+	
+	@RequestMapping(path="/outsidecall" , method= RequestMethod.GET)
+    public String outsidecall() {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "http://httpbin.org/headers";
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        HttpEntity<String> httpEntity =
+            new HttpEntity<>("", httpHeaders);
+
+        String responseBody;
+        try {
+            ResponseEntity<String> response
+            = restTemplate.exchange(url, HttpMethod.GET,httpEntity,String.class);
+            responseBody = response.getBody();
+        } catch (Exception e) {
+            responseBody = e.getMessage();
+        }
+        return responseBody + "\n";
     }
 	
     @RequestMapping(path="/allbrandsui" , method= RequestMethod.GET)

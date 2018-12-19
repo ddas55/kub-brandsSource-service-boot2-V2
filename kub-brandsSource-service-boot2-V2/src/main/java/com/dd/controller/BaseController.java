b0dd99f4dc4cf7650a0e6c803971fabc00771a4a
das <das@das-HP-Pavilion>
2018-12-19 12:48:25 -0800

    c

--- kub-brandsSource-service-boot2-V2/src/main/java/com/dd/controller/BaseController.java
+++ kub-brandsSource-service-boot2-V2/src/main/java/com/dd/controller/BaseController.java
@@ -1,30 +1,10 @@
 package com.dd.controller;
 
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.annotation.security.PermitAll;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import com.dd.dao.ConfigDAO;
-import com.dd.dao.DAOException;
-import com.dd.data.AppStatusInfo;
-import com.dd.data.Brand;
 
 @RestController
 @RequestMapping("/base")
@@ -32,11 +12,6 @@ import com.dd.data.Brand;
 public class BaseController {
 	private static final Logger logger = LoggerFactory.getLogger(BaseController.class);
 
-	@RequestMapping("/")
-	public String healthy() {
-	    return String.valueOf(System.currentTimeMillis());
-	}
-	
 	@RequestMapping("/healthz")
 	public String healthz() {
 	    return String.valueOf(System.currentTimeMillis());
